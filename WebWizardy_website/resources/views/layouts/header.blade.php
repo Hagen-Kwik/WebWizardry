c06a43490913b6358f45039483460819d3ec23ed
reefuu <rifqiet@gmail.com>
2023-11-13 15:48:26 +0700

    heder

--- WebWizardy_website/resources/views/layouts/header.blade.php
+++ WebWizardy_website/resources/views/layouts/header.blade.php
@@ -8,9 +8,14 @@
                     </a>
 
                     <ul class="nav">
-                        <li class=""><a href="/" class="active">Home</a></li>
-                        <li class=""><a href="/pricing">Pricing</a></li>
-                        <li class=""><a href="/past_projects">Past Projects</a></li>
+                        <li class=""><a href="/" class="{{ Request::is('/') ? 'active' : '' }}">Home</a>
+                        </li>
+                        <li class=""><a href="/pricing"
+                                class="{{ Request::is('pricing') ? 'active' : '' }}">Pricing</a>
+                        </li>
+                        <li class=""><a href="/past_projects"
+                                class="{{ Request::is('past_projects') ? 'active' : '' }}">Past
+                                Projects</a></li>
                         {{-- <li class=""><a href="/about_us">About Us</a></li> --}}
                         <li>
                             <div class="gradient-button"><a id="" href=""><i
