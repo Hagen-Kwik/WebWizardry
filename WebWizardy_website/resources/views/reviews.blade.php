93e72cac11039e0c8c2dac40a5eaea57f9805658
HansJoachimW <hansjw123@gmail.com>
2023-11-14 08:33:06 +0700

    review

--- WebWizardy_website/resources/views/reviews.blade.php
+++ WebWizardy_website/resources/views/reviews.blade.php
@@ -1,95 +1,14 @@
 @extends('layouts.main_layout')
 
-@section('title', 'Web Wizardry')
+@section('title', 'Reviews')
 
 @section('content')
-
-
-    <!-- ***** Banner ***** -->
-    <div class="main-banner wow fadeIn" id="top" data-wow-duration="1s" data-wow-delay="0.5s">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="row">
-                        <div class="col-lg-6 align-self-center">
-                            <div class="left-content show-up header-text wow fadeInLeft" data-wow-duration="1s"
-                                data-wow-delay="1s">
-                                <div class="row">
-                                    <div class="col-lg-12">
-                                        <h2>Create Your Very Own Website Now!</h2>
-                                        <p>Use WebWizardry to create the your dream website with ease</p>
-                                    </div>
-
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="right-image wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.5s">
-                                <img src="all_images/banner.png" alt="">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- ***** Features ***** -->
-    <div id="services" class="services section">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 offset-lg-2">
-                    <div class="section-heading  wow fadeInDown" data-wow-duration="1s" data-wow-delay="0.5s">
-                        <h4>Amazing <em>Services &amp; Features</em> for you</h4>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-3">
-                    <div class="service-item first-service">
-                        <div class="icon"></div>
-                        <h4>Progress Tracking</h4>
-                        <p>Track the progress of your website building process inside this website directly</p>
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="service-item second-service">
-                        <div class="icon"></div>
-                        <h4>Rocket Fast Production</h4>
-                        <p>Your dream website creation will take significantly less time than our competitors.</p>
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="service-item third-service">
-                        <div class="icon"></div>
-                        <h4>Negotiable Pricing</h4>
-                        <p>Enjoy negotiable pricing options for flexible payment plans that suit your budget and project
-                            needs.</p>
-
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="service-item fourth-service">
-                        <div class="icon"></div>
-                        <h4>Real-time Communication</h4>
-                        <p>Enable real-time communication in website between collaborators for seamless collaboration</p>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- ***** Testimonials ***** -->
-    {{-- Make fake clients --}}
-    <div id="clients" class="the-clients">
+<div id="clients" class="the-clients">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8 offset-lg-2">
                     <div class="section-heading">
-                        <h4>Check What <em>The Clients Say</em> About Our Service</h4>
+                        <h4>Check What <em>The Clients Say</em> About Our App Dev</h4>
                         <img src="images/heading-line-dec.png" alt="">
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eismod tempor incididunt ut
                             labore et dolore magna.</p>
@@ -105,11 +24,11 @@
                                             <div class="thumb">
                                                 <div class="row">
                                                     <div class="col-lg-4 col-sm-4 col-12">
-                                                        <h4>Name</h4>
-                                                        <span class="date">14 November 2023</span>
+                                                        <h4>David Martino Co</h4>
+                                                        <span class="date">30 November 2021</span>
                                                     </div>
                                                     <div class="col-lg-4 col-sm-4 d-none d-sm-block">
-                                                        <span class="category">Citizen Journalism Remaja</span>
+                                                        <span class="category">Financial Apps</span>
                                                     </div>
                                                     <div class="col-lg-4 col-sm-4 col-12">
                                                         <i class="fa fa-star"></i>
@@ -122,7 +41,7 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        {{-- <div>
+                                        <div>
                                             <div class="thumb">
                                                 <div class="row">
                                                     <div class="col-lg-4 col-sm-4 col-12">
@@ -142,8 +61,8 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div> --}}
-                                        {{-- <div>
+                                        </div>
+                                        <div>
                                             <div class="thumb">
                                                 <div class="row">
                                                     <div class="col-lg-4 col-sm-4 col-12">
@@ -163,8 +82,8 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div> --}}
-                                        {{-- <div>
+                                        </div>
+                                        <div>
                                             <div class="thumb">
                                                 <div class="row">
                                                     <div class="col-lg-4 col-sm-4 col-12">
@@ -184,13 +103,13 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div> --}}
-                                        {{-- <div class="last-thumb">
+                                        </div>
+                                        <div class="last-thumb">
                                             <div class="thumb">
                                                 <div class="row">
                                                     <div class="col-lg-4 col-sm-4 col-12">
                                                         <h4>Mark Amber Do</h4>
-                                                        <span class="date">14 November 2023</span>
+                                                        <span class="date">21 November 2021</span>
                                                     </div>
                                                     <div class="col-lg-4 col-sm-4 d-none d-sm-block">
                                                         <span class="category">Web Development</span>
@@ -205,12 +124,12 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div> --}}
+                                        </div>
                                     </div>
                                 </div>
                                 <div class="col-lg-5">
                                     <ul class="nacc">
-                                        {{-- <li class="active">
+                                        <li class="active">
                                             <div>
                                                 <div class="thumb">
                                                     <div class="row">
@@ -234,8 +153,8 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </li> --}}
-                                        {{-- <li>
+                                        </li>
+                                        <li>
                                             <div>
                                                 <div class="thumb">
                                                     <div class="row">
@@ -259,8 +178,8 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </li> --}}
-                                        {{-- <li>
+                                        </li>
+                                        <li>
                                             <div>
                                                 <div class="thumb">
                                                     <div class="row">
@@ -284,8 +203,8 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </li> --}}
-                                        {{-- <li>
+                                        </li>
+                                        <li>
                                             <div>
                                                 <div class="thumb">
                                                     <div class="row">
@@ -309,7 +228,7 @@
                                                     </div>
                                                 </div>
                                             </div>
-                                        </li> --}}
+                                        </li>
                                         <li>
                                             <div>
                                                 <div class="thumb">
@@ -344,82 +263,4 @@
             </div>
         </div>
     </div>
-
-    <div id="pricing" class="pricing-tables">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8 offset-lg-2">
-                    <div class="section-heading">
-                        <h4>We Have The Best  <em>Prices</em> You Can Get</h4>
-                    </div>
-                </div>
-
-                {{-- first --}}
-                <div class="col-lg-4">
-                    <div class="pricing-item-regular">
-                        <span class="price">1 JT</span>
-                        <h4>Single-Page Website</h4>
-                        <div class="icon">
-                            <img src="images/pricing-table-01.png" alt="">
-                        </div>
-                        <ul>
-                            <li>1 Long Page</li>
-                            <li>Pre-Made Assets</li>
-                            <li class="non-function">Maintenence</li>
-                            <li class="non-function">Premium Add-Ons</li>
-                            <li class="non-function">Hosting Avialable</li>
-                        </ul>
-                        <div class="border-button">
-                            <a href="/pricing">See Details</a>
-                        </div>
-                    </div>
-                </div>
-
-                {{-- second --}}
-                <div class="col-lg-4">
-                    <div class="pricing-item-pro">
-                        <span class="price">5 JT</span>
-                        <h4>Personal Website</h4>
-                        <div class="icon">
-                            <img src="images/pricing-table-01.png" alt="">
-                        </div>
-                        <ul>
-                            <li>Up to 10 Pages</li>
-                            <li>Pre-Made Assets</li>
-                            <li>Maintenence</li>
-                            <li>Limited Premium Add-Ons</li>
-                            <li class="non-function">Hosting Avialable</li>
-                        </ul>
-                        <div class="border-button">
-                            <a href="/pricing">See Details</a>
-                        </div>
-                    </div>
-                </div>
-
-                {{-- third --}}
-                <div class="col-lg-4">
-                    <div class="pricing-item-regular">
-                        <span class="price">10 JT</span>
-                        <h4>Professional Website</h4>
-                        <div class="icon">
-                            <img src="images/pricing-table-01.png" alt="">
-                        </div>
-                        <ul>
-                            <li>Unlimited Pages</li>
-                            <li>Hand-Made Assets</li>
-                            <li>Maintenence per month</li>
-                            <li>Premium Add-Ons</li>
-                            <li>Hosting Avialable</li>
-                        </ul>
-                        <div class="border-button">
-                            <a href="/pricing">See Details</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-
 @endsection
